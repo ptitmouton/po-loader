d9228b129198e53de635c678be1914061e520ea4
Eemeli Aro <eemeli@gmail.com>
2018-03-23 17:48:33 +0200

    Update example for new API

--- example/src/index.js
+++ example/src/index.js
@@ -1,10 +1,12 @@
-import messages from './messages.po'
+import pl from './messages.po'
+import MessageFormat from 'messageformat/lib/get'
+const messages = new MessageFormat({ pl })
 
 function component() {
   const element = document.createElement('div')
-  element.innerHTML = messages['%s took %d ms to complete.'](['TASK', 42])
+  element.innerHTML = messages.get('%s took %d ms to complete.', ['TASK', 42])
   return element
 }
 
-console.log('messages', messages)
+console.log('messages', messages.get([]))
 document.body.appendChild(component())
